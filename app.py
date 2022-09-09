f1b4fee20ef7263bf920bf28a1452c2afb0f4134
User <r121221@yandex.ru>
2022-09-09 11:30:56 +0300

    init

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Мое первое приложение на фласке!!</h1> <p> Вторая строчка </p>' \
+           '<table><table border=#><tr><td>ячейка 1</td><td>ячейка 2</td></tr></table> '
+
+@app.route('/privet')
+def greeting():
+    return '<h2> Здравствуй милый друг </h2>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
