dc4ce651c5ff39d51a38c8cd795b3d46546773e6
User <r121221@yandex.ru>
2022-09-09 11:44:15 +0300

    рпмьтми

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Мое первое приложение на фласке!!</h1> <p> Вторая строчка </p>' \
+    return 'ываываыва<h1>Мое первое приложение на фласке!!</h1> <p> Вторая строчка </p>' \
            '<table><table border=#><tr><td>ячейка 1</td><td>ячейка 2</td></tr></table> '
 
 @app.route('/privet')
