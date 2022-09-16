1d028a50d8c5349c8de83c5e2526552a3ff3d42f
User <r121221@yandex.ru>
2022-09-16 10:33:13 +0300

    добавил динамические страницы

--- app.py
+++ app.py
@@ -8,9 +8,20 @@ def hello():
     return 'ываываыва<h1>Мое первое приложение на фласке!!</h1> <p> Вторая строчка </p>' \
            '<table><table border=#><tr><td>ячейка 1</td><td>ячейка 2</td></tr></table> '
 
-@app.route('/privet')
+
+@app.route('/privet/')
 def greeting():
     return '<h2> Здравствуй милый друг </h2>'
+
+
+@app.route('/<user>/')
+def users(user):
+    return f'<h2> Здравствуй наш пользователь {user} </h2>'
+
+
+@app.route('/<int:id>/')
+def users_id(id):
+    return f'<h2> Ваш номер после номера {id} </h2>'
 
 
 if __name__ == '__main__':
