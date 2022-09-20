04429cf47adc1a54767d431e44394132c4a583fe
User <mickky@list.ru>
2022-09-20 12:40:20 +0300

    добавил разденеие по группам

---
+++ apppri/routes.py
@@ -0,0 +1,6 @@
+from apppri import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
---
+++ wsgi.py
@@ -0,0 +1,8 @@
+# gruup = {1:'ПРи201' }
+GROUP = 1
+if  GROUP == 1:
+    from apppri import app
+
+
+if __name__ == '__main__':
+    app.run()
