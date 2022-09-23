8a530586b8df9c7b4edad0b768f5298c54c685e6
User <r121221@yandex.ru>
2022-09-23 11:30:16 +0300

    добавил список ист 23_09

--- appist/routes.py
+++ appist/routes.py
@@ -5,6 +5,15 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    best_pi = {'username': 'Елизавета'}
+    best_ist = {'username': 'Виктория'}
+    favorite_writes = [{'author': {'username': 'Tolkien'},
+              'body': ' Lords of the ring'
+                       },
+                      {'author': {'username': 'Pushkin'},
+              'body': ' Capitans of the daughter'
+              },
+                      {'author': {'username': 'Lermontov'},
+              'body': ' Парус'
+              }]
 
-    return render_template('index.html', title='2022 Forever', user=best_pi)
+    return render_template('index.html', title='2022 Forever', user=best_ist, favorite_writes=favorite_writes)
