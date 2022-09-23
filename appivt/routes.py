ab201b675dd0841191d1d8daa1a42d9204caa4ef
User <mickky@list.ru>
2022-09-23 13:04:53 +0300

    ивт 23_09 шаблон

--- appivt/routes.py
+++ appivt/routes.py
@@ -3,9 +3,9 @@ from flask import render_template
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
-    best_ist = {'username': 'Виктория'}
+    best_ivt = {'username': 'Шляпкин'}
     favorite_writes = [{'author': {'username': 'Tolkien'},
               'body': ' Lords of the ring'
                        },
@@ -16,4 +16,15 @@ def index():
               'body': ' Парус'
               }]
 
-    return render_template('index.html', title='2022 Forever', user=best_ist, favorite_writes=favorite_writes)
+    return render_template('index.html', title='2022 Forever', user=best_ivt, favorite_writes=favorite_writes)
+
+
+@app.route('/dishes/')
+def dish():
+   best_user = {'username' : 'Николай'}
+   favorite_dishes = [{'name': {'dishname': 'Fried chicken'},
+                       'ingridients':{'ingr1': 'Meat of chicken',
+                                      'ingr2' : 'some spicy sauce'},
+                       'photo': 'https://hi-news.ru/wp-content/uploads/2020/06/chicken_home_image_one-750x558.jpg'}]
+
+   return render_template('dishes.html', title='2022 Forever', abuser=best_user, favorite_dishes=favorite_dishes)
