bced5050675ec012cce4845cebc911cf23220c93
User <mickky@list.ru>
2022-09-27 13:25:00 +0300

    pri ___

--- apppri/routes.py
+++ apppri/routes.py
@@ -1,6 +1,10 @@
 from apppri import app
 from flask import render_template
 
+menu = [{"name": "начало", "url": "index"},
+        {"name": "Главная", "url": "main"},
+        ]
+
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +12,8 @@ def index():
     best_pri = {'username': 'Макарцев'}
 
     return render_template('index.html', title='2022 Forever', user=best_pri)
+
+
+@app.route('/main')
+def main():
+    return render_template('main.html', menu=menu, title= 'FJKHSF:OIUJFWIJHF')
