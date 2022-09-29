fed68c3f1c69fd80429d3664fc5427d60d31a491
User <r121221@yandex.ru>
2022-09-29 12:15:02 +0300

    pi 26/09/2020

--- apppi/routes.py
+++ apppi/routes.py
@@ -8,3 +8,8 @@ def index():
     best_pi = {'username': 'Елизавета'}
 
     return render_template('index.html', title='2022 Forever', user=best_pi)
+
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
