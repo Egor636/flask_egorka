6875cde1c12ecd62111882e501eed57955666314
User <mickky@list.ru>
2022-09-30 13:06:53 +0300

    ivt 30_09_2022

--- appivt/routes.py
+++ appivt/routes.py
@@ -1,30 +1,39 @@
 from appivt import app
 from flask import render_template
 
+menu = [{'name': 'Главная', 'url': 'index'}, {'name': 'Блюда', 'url': 'dishes'}, {'name': 'Помощь', 'url': 'help'}]
+
 
 @app.route('/')
-@app.route('/index/')
+@app.route('/index')
 def index():
     best_ivt = {'username': 'Шляпкин'}
     favorite_writes = [{'author': {'username': 'Tolkien'},
-              'body': ' Lords of the ring'
-                       },
-                      {'author': {'username': 'Pushkin'},
-              'body': ' Capitans of the daughter'
-              },
-                      {'author': {'username': 'Lermontov'},
-              'body': ' Парус'
-              }]
+                        'body': ' Lords of the ring'
+                        },
+                       {'author': {'username': 'Pushkin'},
+                        'body': ' Capitans of the daughter'
+                        },
+                       {'author': {'username': 'Lermontov'},
+                        'body': ' Парус'
+                        }]
 
-    return render_template('index.html', title='2022 Forever', user=best_ivt, favorite_writes=favorite_writes)
+    return render_template('index.html', title='2022 Forever', user=best_ivt, favorite_writes=favorite_writes,
+                           menu=menu)
 
 
-@app.route('/dishes/')
+@app.route('/dishes')
 def dish():
-   best_user = {'username' : 'Николай'}
-   favorite_dishes = [{'name': {'dishname': 'Fried chicken'},
-                       'ingridients':{'ingr1': 'Meat of chicken',
-                                      'ingr2' : 'some spicy sauce'},
-                       'photo': 'https://hi-news.ru/wp-content/uploads/2020/06/chicken_home_image_one-750x558.jpg'}]
+    best_user = {'username': 'Николай'}
+    favorite_dishes = [{'name': {'dishname': 'Fried chicken'},
+                        'ingridients': {'ingr1': 'Meat of chicken',
+                                        'ingr2': 'some spicy sauce'},
+                        'photo': 'https://hi-news.ru/wp-content/uploads/2020/06/chicken_home_image_one-750x558.jpg'}]
 
-   return render_template('dishes.html', title='2022 Forever', abuser=best_user, favorite_dishes=favorite_dishes)
+    return render_template('dishes.html', title='2022 Forever', abuser=best_user, favorite_dishes=favorite_dishes,
+                           menu=menu)
+
+
+@app.route('/help')
+def help():
+    return render_template('help.html', title='Cправка', menu=menu)
