caea7966c29fced6d1dc2b2e2d1d6b3adcf0f504
MichaelMr12 <r121221@yndex.ru>
2022-10-11 22:25:53 +0300

    pri конфиг

--- apppri/__init__.py
+++ apppri/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from apppri.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ikljsdehgfno;slkefjhgwielkjtnmgfwoiehjnewrkgn'
+# app.config['SECRET_KEY'] = 'ikljsdehgfno;slkefjhgwielkjtnmgfwoiehjnewrkgn'
+
+
+app.config.from_object(Con)
 
 from apppri import routes
