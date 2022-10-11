896cefd42c6fe0c5ef2515a3ae27ecb623ab64ea
User <mickky@list.ru>
2022-10-11 13:25:14 +0300

    pri 11_10_2022

--- apppri/routes.py
+++ apppri/routes.py
@@ -1,10 +1,11 @@
 from apppri import app
-from flask import render_template
+from flask import render_template, request, flash
 
 menu = [{"title": "Начало", "url": "index"},
         {"title": "Главная", "url": "main"},
         {"title": "Помощь", "url": "help"},
         {"title": "О приложении", "url": "about"},
+        {"title": "Обратная связь", "url": "callback"}
         ]
 
 
@@ -13,7 +14,7 @@ menu = [{"title": "Начало", "url": "index"},
 def index():
     best_pri = {'username': 'Макарцев'}
 
-    return render_template('index.html', title='2022 Forever', user=best_pri)
+    return render_template('index.html', title='2022 Forever', menu=menu, user=best_pri)
 
 
 @app.route('/help')
@@ -26,7 +27,18 @@ def about():
     return render_template('help.html', title='C горем попалам ПРИ', menu=menu)
 
 
-
 @app.route('/main')
 def main():
     return render_template('main.html', menu=menu, title='Главная')
+
+
+@app.route('/callback', methods=["POST", "GET"])
+def callback():
+    if request.method == 'POST':
+        if len(request.form['username']) > 2 and '@' in request.form['email']:
+            flash('Сообщение отправлено')
+        else:
+            flash('Ошибка отправки')
+        print(request.form)
+        print(request.form['email'])
+    return render_template('callback.html', menu=menu, title="Обратная связь")
