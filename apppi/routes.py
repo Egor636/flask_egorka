d798af18ae64db8eada238d31702f439ab173588
User <r121221@yandex.ru>
2022-10-13 13:21:11 +0300

    13_10 pi base

--- apppi/routes.py
+++ apppi/routes.py
@@ -2,20 +2,21 @@ from apppi import app
 from flask import render_template
 from random import choice
 
-menu = ['Главная', 'Помощь', 'О программе']
+menu = [{"name": 'Главная', "url": 'index'}, {"name": 'О программе', "url": 'about'}, {"name": 'Помощь', "url": 'help'}]
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     best_pi = {'username': 'Елизавета'}
 
-    return render_template('index.html', title='2022 Forever', user=best_pi)
+    return render_template('index.html', title='2022 Forever', user=best_pi, menu=menu)
 
 
 @app.route('/help')
 def help():
     sp = ['PI', '2014', '']
-    return render_template('help.html', title=choice(sp))
+    return render_template('help.html', title=choice(sp), menu=menu)
 
 
 @app.route('/about')
