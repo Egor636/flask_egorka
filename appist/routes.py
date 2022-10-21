14f257b7de632af17c87320b603df73d7138cf67
User <r121221@yandex.ru>
2022-10-21 11:39:32 +0300

    ist 21.10 дз 1создать обработчик ошибки 401  2 сохранить пароли и логины в коллекции и изменить обработчик login

--- appist/routes.py
+++ appist/routes.py
@@ -1,8 +1,8 @@
 from appist import app
-from flask import render_template, request, flash, get_flashed_messages
+from flask import render_template, request, flash, get_flashed_messages, session, redirect, url_for, abort
 
 menu = [{'name': 'Главная', 'url': 'index'}, {'name': 'Помощь', 'url': 'help'},
-        {'name': 'Обратная связь', 'url': 'contact'}]
+        {'name': 'Обратная связь', 'url': 'contact'}, {'name': 'Авторизация', 'url': 'login'}]
 
 
 @app.route('/')
@@ -31,7 +31,7 @@ def help():
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        if len(request.form['username']) >=2:
+        if len(request.form['username']) >= 2:
             flash('Сообщение отправлено', category='success')
             print(get_flashed_messages(True))
             print(request.form)
@@ -40,3 +40,25 @@ def contact():
             flash("Ошибка отправки", category='error')
 
     return render_template('contact.html', title=' Контакт', menu=menu)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if 'userlogged' in session:
+        return redirect(url_for('profile', username=session['userlogged']))
+    elif request.method == "POST" and request.form['username'] == '1' and request.form['psw'] == '2':
+        session['userlogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userlogged']))
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    if 'userlogged' not in session or session['userlogged'] != username:
+        abort(401)
+    return f"<h1> Пользователь: {username} </h1>"
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html', title='Внимание страница не найдена', menu=menu, error=error), 404
