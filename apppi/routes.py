9da858c75bca9ac126772d92c48c8e90e825631c
Michael <r121221@yandex.ru>
2022-10-25 13:34:37 +0300

    добавить sql

--- apppi/routes.py
+++ apppi/routes.py
@@ -2,13 +2,13 @@ from apppi import app
 from flask import render_template
 from random import choice
 
-menu = [{"name": 'Главная', "url": 'index'}, {"name": 'О программе', "url": 'about'}, {"name": 'Помощь', "url": 'help'}]
+menu = [{"name": 'Главная', "url": 'index'}, {"name": 'О программе', "url": 'about'}, {"name": 'Помощь', "url": 'help'}, {"name": 'о бо мне ', "url": 'me'}]
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    best_pi = {'username': 'Елизавета'}
+    best_pi = {'username': 'Егор'}
 
     return render_template('index.html', title='2022 Forever', user=best_pi, menu=menu)
 
@@ -22,3 +22,7 @@ def help():
 @app.route('/about')
 def about():
     return render_template('about.html', menu=menu)
+
+@app.route('/me')
+def me():
+    return render_template('me.html', menu=menu)
